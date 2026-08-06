f4134f5af98d52a3865c3487bdc051dae842a55a
KajaJohanne <kajajohanne@gmail.com>
2026-08-06 12:15:19 +0200

    feature hente og vis lagrede mønster på HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { getPatterns } from "../api/patterns";
+import { Link } from "react-router-dom";
+import type { Pattern } from "../types/pattern";
+
 export function HomePage() {
+  const [patterns, setPatterns] = useState<Pattern[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function loadPatterns() {
+    const data = await getPatterns();
+    setPatterns(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    loadPatterns();
+  });
+
   return (
     <div>
       <h1>Dette er landingssiden. Her skal tidligere mønstre vises</h1>
       <p>:)</p>
+
+      {isLoading && <p>Laster..</p>}
+
+      {!isLoading &&
+        patterns.map((pattern) => <p key={pattern.id}>{pattern.name}</p>)}
     </div>
   );
 }
