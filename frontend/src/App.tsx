9afef4ae7c93bb07dc16fe7b4501cc71cf991630
KajaJohanne <kajajohanne@gmail.com>
2026-09-03 19:40:43 +0200

    feat endret fra alert til toast ved lagring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { NewPatternPage } from "./pages/NewPatternPage";
 import { EditorPage } from "./pages/EditorPage";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen">
+        <Toaster position="top-center" richColors />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/new" element={<NewPatternPage />} />
